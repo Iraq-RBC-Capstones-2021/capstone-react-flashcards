f58de08f369f9c8b38264b6cf0792dc00893044a
GE47 <hassan2pac47@gmail.com>
2021-10-04 23:15:50 +0300

    Extend tailwind with the required colors and fonts
    Fixes #4

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      primary: "#FF886C",
+      accent: "#FBEDE6",
+      gray: "#E4E4E7",
+      black: "#1A1A1A",
+      aqua: "#1890FF",
+      grass: "#52C41A",
+    },
+    fontFamily: {
+      rubik: ["Rubik", "sans-serif"],
+    },
   },
   variants: {
     extend: {},
