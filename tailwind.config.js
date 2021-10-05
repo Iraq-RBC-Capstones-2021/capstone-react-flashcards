efbd01f6d3c9827be252921431642c4b23e1d02e
GE47 <hassan2pac47@gmail.com>
2021-10-05 19:08:45 +0300

    make tailwind colors extend the actual colors instead of overwrite them
    Fixes #11

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,18 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      primary: "#FF886C",
-      accent: "#FBEDE6",
-      gray: "#E4E4E7",
-      black: "#1A1A1A",
-      aqua: "#1890FF",
-      grass: "#52C41A",
-    },
-    fontFamily: {
-      rubik: ["Rubik", "sans-serif"],
+    extend: {
+      colors: {
+        primary: "#FF886C",
+        accent: "#FBEDE6",
+        gray: "#E4E4E7",
+        black: "#1A1A1A",
+        aqua: "#1890FF",
+        grass: "#52C41A",
+      },
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
     },
   },
   variants: {
