a7fb24b1242c3c8898c592d2c0969d98d7291a54
Yasser Muayed <yassermuayed@gmail.com>
2021-10-06 21:31:12 +0300

    fix link to home page

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 export default function notFound() {
   return (
@@ -9,7 +10,7 @@ export default function notFound() {
         else.
       </p>
       <button className="shadow-lg my-2 px-3 py-1.5 font-semibold text-base border-black border-solid border-2 rounded-lg ">
-        Go To Home
+        <Link href="/">Go To Home</Link>
       </button>
     </div>
   );
