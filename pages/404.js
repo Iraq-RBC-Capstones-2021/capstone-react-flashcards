2fdc31212bf1dce15587f41ca0ff3d594d7efdc0
Yasser Muayed <yassermuayed@gmail.com>
2021-10-06 21:24:16 +0300

    create the 404 page Fixes #14

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function notFound() {
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <h1 className="font-semibold text-2xl">404</h1>
+      <p className="mx-4 w-96 text-center text-xl">
+        Sorry, the page you wanted was not found. try serching for something
+        else.
+      </p>
+      <button className="shadow-lg my-2 px-3 py-1.5 font-semibold text-base border-black border-solid border-2 rounded-lg ">
+        Go To Home
+      </button>
+    </div>
+  );
+}
