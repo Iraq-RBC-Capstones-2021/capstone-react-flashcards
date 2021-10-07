04031c46fce002d535cd599a45827feab0d81042
Yasser Muayed <yassermuayed@gmail.com>
2021-10-07 18:40:34 +0300

    add btn-secondary styling to button

--- components/Section.js
+++ components/Section.js
@@ -59,7 +59,7 @@ export default function Section({
             objectFit="contain"
           />
           {action ? (
-            <button className=' absolute  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"'>
+            <button className='btn-secondary absolute  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"'>
               <Link href={`${url}`}>Preview</Link>
               {"   "}
               <Image
