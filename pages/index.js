a402abdc19801955bbf2b5ae7d1f6ac84f3ea8fd
Lana Kamal <lana.km98@gmail.com>
2021-10-19 11:12:14 +0300

    remove unessessary code

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,10 @@ export default function Home() {
 
       <div>
         <div
-          className="flex bg-top bg-cover relative justify-center bg-no-repeat w-full p-60"
+          className="flex bg-top bg-cover justify-center bg-no-repeat w-full p-60"
           style={{ backgroundImage: `url(/assets/background-landingPage.png)` }}
         >
-          <div className="flex relative flex-col justify-center items-center z-10">
+          <div className="flex flex-col justify-center items-center">
             <h1 className="font-bold text-4xl pb-4">Make Flashcards Easily</h1>
             <p className="mx-4 w-96 text-center text-lg mb-8 opacity-50">
               Create, browse and study different flashcards for studying or
@@ -28,18 +28,6 @@ export default function Home() {
             <Link href="/register">
               <a className="btn-secondary">Get Started</a>
             </Link>
-          </div>
-
-          <div className="hidden block absolute top-40 left-24 transform -rotate-12">
-            <Card />
-          </div>
-
-          <div className="hidden block absolute bottom-0 right-28 transform rotate-45">
-            <Card />
-          </div>
-
-          <div className="hidden block absolute top-10 right-44 transform rotate-6">
-            <Card />
           </div>
         </div>
 
