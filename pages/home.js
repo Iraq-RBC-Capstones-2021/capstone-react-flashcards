08195b297e4d1c6decfca7142dd23a8ba1b34c8b
ulfetalhussaini <ulfetalhussaini@yahoo.com>
2021-10-21 08:06:31 +0300

    Fix home page styling Fixes #65

--- pages/home.js
+++ pages/home.js
@@ -2,7 +2,7 @@ import styles from "../styles/Home.module.css";
 import Carsouel from "../components/CarouselPage";
 import Category from "../components/Category";
 import Card from "../components/Card";
-import data from "../sets.json";
+import data from "../sets.js";
 import { SwiperSlide } from "swiper/react";
 
 export default function Home() {
@@ -13,7 +13,7 @@ export default function Home() {
 
   const allSets = (set) => {
     return (
-      <SwiperSlide>
+      <SwiperSlide key={set.id}>
         <Card
           key={set.id}
           userName={set.userName}
